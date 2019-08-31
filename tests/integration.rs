b06366b3980fd385b8d3217d157a89ca8362e1b2
madprops <easywest@protonmail.com>
2019-08-31 16:47:44 -0600

    Test mod

--- tests/integration.rs
+++ tests/integration.rs
@@ -3,16 +3,16 @@ use colorskill::*;
 #[test]
 fn integration_test()
 {
-    let c = (100, 100, 100);
-    assert_eq!(make_color_darker(c, 20.0), (49, 49, 49));
-    assert_eq!(make_color_lighter(c, 20.0), (151, 151, 151));
+    let c = (100, 110, 120);
+    assert_eq!(make_color_darker(c, 20.0), (54, 59, 64));
+    assert_eq!(make_color_lighter(c, 20.0), (152, 161, 170));
 
-    let mut c2 = RGB::new(100, 100, 100);
+    let mut c2 = RGB::new(100, 110, 120);
     c2.change("darker2");
-    assert_eq!(c2.get_tuple(), (49, 49, 49));
-    assert_eq!(c2.get_hue(), 0.0);
-    assert_eq!(c2.get_saturation(), 0.0);
-    assert_eq!(c2.get_lightness(), 19.22);
+    assert_eq!(c2.get_tuple(), (54, 59, 64));
+    assert_eq!(c2.get_hue(), 210.0);
+    assert_eq!(c2.get_saturation(), 8.47);
+    assert_eq!(c2.get_lightness(), 23.14);
 
     c2.randomize();
 }
